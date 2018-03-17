18eff2c4099ff845ecc89a371798cfc2510f0516
EaterOfHope <nathan.mack10@gmail.com>
2018-03-16 22:37:21 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,19 +1,3 @@
-# Email Scraper: a script for scraping the internet for email addresses.
-#     Copyright (C) 2017 Miles McCain
-#
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import requests
 import sys
 import random
